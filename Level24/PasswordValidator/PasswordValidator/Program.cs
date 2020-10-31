ad2ecc8f07ea86f70a23cbf62a94fea5eea8382b
RB Whitaker <RB.Whitaker@asirobots.com>
2020-10-31 16:24:54 -0600

    Added in sample code for 'The Password Validator'.

---
+++ Level24/PasswordValidator/PasswordValidator/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace PasswordValidator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PasswordValidator validator = new PasswordValidator();
+
+            while (true)
+            {
+                Console.Write("Enter a password: ");
+                string password = Console.ReadLine();
+
+                if (validator.IsValid(password))
+                    Console.WriteLine("That password meets all criteria.");
+                else
+                    Console.WriteLine("That password does not meet all criteria.");
+            }
+        }
+    }
+}
