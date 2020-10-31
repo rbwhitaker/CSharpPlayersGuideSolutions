6de9e4b9268ea13657afbd7a7aaa21359f5bfba6
RB Whitaker <RB.Whitaker@asirobots.com>
2020-10-31 09:36:39 -0600

    Added a solution for the challenge 'The Point'.

---
+++ Level24/ThePoint/ThePoint/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Point
+{
+    public class Point
+    {
+        public double X { get; }
+        public double Y { get; }
+
+        public Point(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public Point() : this(0, 0) { }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point p1 = new Point(2, 3);
+            Point p2 = new Point(-4, 0);
+
+            Console.WriteLine($"({p1.X}, {p1.Y})");
+            Console.WriteLine($"({p2.X}, {p2.Y})");
+        }
+    }
+}
