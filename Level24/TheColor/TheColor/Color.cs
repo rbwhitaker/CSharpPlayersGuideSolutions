f281b32fd98a36c8b00d25693cd8137e9becbdd2
RB Whitaker <RB.Whitaker@asirobots.com>
2020-10-31 10:01:17 -0600

    Added code for 'The Color' Challenge.

---
+++ Level24/TheColor/TheColor/Color.cs
@@ -0,0 +1,25 @@
+﻿namespace TheColor
+{
+    public class Color
+    {
+        public byte R { get; }
+        public byte G { get; }
+        public byte B { get; }
+
+        public Color(byte r, byte g, byte b)
+        {
+            R = r;
+            G = g;
+            B = b;
+        }
+
+        public static Color White { get; } = new Color(255, 255, 255);
+        public static Color Black { get; } = new Color(0, 0, 0);
+        public static Color Red { get; } = new Color(255, 0, 0);
+        public static Color Orange { get; } = new Color(255, 165, 0);
+        public static Color Yellow { get; } = new Color(255, 255, 0);
+        public static Color Green { get; } = new Color(0, 128, 0);
+        public static Color Blue { get; } = new Color(0, 0, 255);
+        public static Color Purple { get; } = new Color(128, 0, 128);
+    }
+}
