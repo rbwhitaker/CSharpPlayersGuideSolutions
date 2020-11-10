e6e3f488d3a51c4e0c81ac83c982a0f2c0dd676d
RB Whitaker <RB.Whitaker@asirobots.com>
2020-11-09 21:20:34 -0700

    Completed the Small, Medium, or Large challenge.

---
+++ Level30/TheFountainOfObjects/TheFountainOfObjects/LevelGenerator.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace TheFountainOfObjects
+{
+    public static class LevelGenerator
+    {
+        public static FountainOfObjectsGame GenerateGame(FountainOfObjectsGame game)
+        {
+            while (game == null)
+            {
+                ConsoleHelper.Write("Do you want to play a small, medium, or large game? ", ConsoleColor.White);
+                game = Console.ReadLine() switch
+                {
+                    "small" => CreateSmallGame(),
+                    "medium" => CreateMediumGame(),
+                    "large" => CreateLargeGame(),
+                    _ => null
+                };
+            }
+
+            return game;
+        }
+
+        public static FountainOfObjectsGame CreateSmallGame()
+        {
+            Map map = new Map(4, 4);
+            map.SetRoomTypeAt(1, 1, RoomType.Fountain);
+
+            map.SetRoomTypeAt(0, 0, RoomType.Entrance);
+            Player player = new Player();
+            player.Row = 0;
+            player.Column = 0;
+
+            return new FountainOfObjectsGame(player, map);
+        }
+
+        public static FountainOfObjectsGame CreateMediumGame()
+        {
+            Map map = new Map(6, 6);
+            map.SetRoomTypeAt(4, 2, RoomType.Fountain);
+
+            map.SetRoomTypeAt(0, 5, RoomType.Entrance);
+            Player player = new Player();
+            player.Row = 0;
+            player.Column = 5;
+
+            return new FountainOfObjectsGame(player, map);
+        }
+
+        public static FountainOfObjectsGame CreateLargeGame()
+        {
+            Map map = new Map(8, 8);
+            map.SetRoomTypeAt(4, 2, RoomType.Fountain);
+
+            map.SetRoomTypeAt(3, 7, RoomType.Entrance);
+            Player player = new Player();
+            player.Row = 3;
+            player.Column = 7;
+
+            return new FountainOfObjectsGame(player, map);
+        }
+    }
+}
--- Level30/TheFountainOfObjects/TheFountainOfObjects/Program.cs
+++ Level30/TheFountainOfObjects/TheFountainOfObjects/Program.cs
@@ -1,14 +1,14 @@
-﻿namespace TheFountainOfObjects
+﻿using System;
+
+namespace TheFountainOfObjects
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Player player = new Player();
-            Map map = new Map(4, 4);
-            map.SetRoomTypeAt(0, 0, RoomType.Entrance);
-            map.SetRoomTypeAt(1, 1, RoomType.Fountain);
-            FountainOfObjectsGame game = new FountainOfObjectsGame(player, map);
+            FountainOfObjectsGame game = null;
+            game = LevelGenerator.GenerateGame(game);
+
             game.Run();
         }
     }
