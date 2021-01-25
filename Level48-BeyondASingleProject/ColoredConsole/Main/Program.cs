10fe07b925fd24b4816c3e2ee947f4cb4137f999
RB Whitaker <RB.Whitaker@asirobots.com>
2021-01-24 19:37:37 -0700

    This challenge got totally reimagined. This is the fixed solution.

---
+++ Level48-BeyondASingleProject/ColoredConsole/Main/Program.cs
@@ -0,0 +1,5 @@
+﻿using Helpers;
+using System;
+
+string name = ColoredConsole.Prompt("What is your name?");
+ColoredConsole.WriteLine("Hello " + name, ConsoleColor.Green);
--- Level48-BeyondASingleProject/ConsoleHelpers/ConsoleHelpers/Program.cs
+++
@@ -1,5 +0,0 @@
-﻿using Helpers;
-using System;
-
-int number = ConsoleHelpers.AskForNumber("Pick a number between 0 and 10.", 0, 10);
-Console.WriteLine($"You picked {number}.");
