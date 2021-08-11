43ba630689197c27e15169f5cf74aeadae1637e1
RB Whitaker <rbwhitaker@outlook.com>
2021-08-10 19:10:35 -0600

    Added a loop to ensure the user picks a good number, as the Prototype challenge requires.

--- Level11-Loops/ThePrototype/Program.cs
+++ Level11-Loops/ThePrototype/Program.cs
@@ -1,7 +1,12 @@
 ﻿using System;
 
-Console.Write("User 1, enter a number between 0 and 100: ");
-int number = Convert.ToInt32(Console.ReadLine());
+int number;
+do
+{
+    Console.Write("User 1, enter a number between 0 and 100: ");
+    number = Convert.ToInt32(Console.ReadLine());
+}
+while (number < 0 || number > 100);
 
 Console.Clear();
 
