efca9a2456ee45c85d75e8cf203a3d1c6fa684a5
RB Whitaker <rbwhitaker@outlook.com>
2021-09-07 20:29:50 -0600

    Added colors to the magic cannon challenge, as is expected by the objectives in the book.

--- Level11-Loops/TheMagicCannon/Program.cs
+++ Level11-Loops/TheMagicCannon/Program.cs
@@ -2,8 +2,24 @@
 
 for (int number = 1; number <= 100; number++)
 {
-    if (number % 5 == 0 && number % 3 == 0) Console.WriteLine($"{number}: Electric and Fire");
-    else if (number % 5 == 0) Console.WriteLine($"{number}: Electric");
-    else if (number % 3 == 0) Console.WriteLine($"{number}: Fire");
-    else Console.WriteLine($"{number}: Normal");
+    if (number % 5 == 0 && number % 3 == 0)
+    {
+        Console.ForegroundColor = ConsoleColor.Cyan;
+        Console.WriteLine($"{number}: Electric and Fire");
+    }
+    else if (number % 5 == 0)
+    {
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine($"{number}: Electric");
+    }
+    else if (number % 3 == 0)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine($"{number}: Fire");
+    }
+    else
+    {
+        Console.ForegroundColor = ConsoleColor.Gray;
+        Console.WriteLine($"{number}: Normal");
+    }
 }
