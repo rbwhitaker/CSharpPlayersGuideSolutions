8ad711a7e0513627765ee87a607e08fef5809daf
RB Whitaker <rbwhitaker@outlook.com>
2022-08-11 21:12:37 -0600

    Changed the prices in this challenge to be divisible by two to sidestep the issue of fractional or floored values.

--- 5thEdition/Level10-Switches/DiscountedInventory/Program.cs
+++ 5thEdition/Level10-Switches/DiscountedInventory/Program.cs
@@ -20,15 +20,29 @@ string item = itemNumber switch
     7 => "Food Supplies"
 };
 
+// Depending on which version of the book you have, these prices might
+// be slightly different than what you have. I tweaked them here to
+// sidestep the issue of handling fractional values or truncation
+// from integer division.
+//
+// However, if your solution just did plain old integer division
+// and left the prices as they were in your version of the book,
+// or if you changed over to floating-point division to take advantage
+// of fractional values, either of those solutions are just fine.
+//
+// The point of this challenge is to use switches and to get
+// experience modifying a program with new features. The division
+// thing is really just a diversion. (Which is why I opted to sidestep
+// it here by making the numbers all divisible by two.)
 int price = item switch
 {
     "Rope" => 10,
-    "Torches" => 15,
-    "Climbing Equipment" => 25,
-    "Clean Water" => 1,
+    "Torches" => 16,
+    "Climbing Equipment" => 24,
+    "Clean Water" => 2,
     "Machete" => 20,
     "Canoe" => 200,
-    "Food Supplies" => 1
+    "Food Supplies" => 2
 };
 
 Console.Write("What is your name? ");
