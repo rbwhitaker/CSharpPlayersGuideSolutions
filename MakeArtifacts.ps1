da6a6e4b32dc5cec37a1532db3e3aa971bf3bea1
rbwhitaker <rbwhitaker@outlook.com>
2025-03-24 07:48:47 -0600

    Updated the script to point to the renamed folders.

--- MakeArtifacts.ps1
+++ MakeArtifacts.ps1
@@ -7,6 +7,6 @@ mkdir Artifacts
 
 mkdir Artifacts/6thEdition
 git archive -o Artifacts/6thEdition/AllSolutions.zip $tag :'6thEdition'
-git archive -o Artifacts/6thEdition/FinalBattleExpansionPathStart.zip $tag :'6thEdition/Level52-TheFinalBattle/00-ExpansionPath-START HERE'
-git archive -o Artifacts/6thEdition/FinalBattleFinal.zip $tag  :'6thEdition/Level52-TheFinalBattle/00-TheFinalBattle-FINAL VERSION'
-git archive -o Artifacts/6thEdition/FountainOfObjectsFinal.zip $tag :'6thEdition/Level31-TheFountainOfObjects/CombinedGame'
+git archive -o Artifacts/6thEdition/FinalBattleExpansionPathStart.zip $tag :'6thEdition/Level53-TheFinalBattle/00-ExpansionPath-START HERE'
+git archive -o Artifacts/6thEdition/FinalBattleFinal.zip $tag  :'6thEdition/Level53-TheFinalBattle/00-TheFinalBattle-FINAL VERSION'
+git archive -o Artifacts/6thEdition/FountainOfObjectsFinal.zip $tag :'6thEdition/Level32-TheFountainOfObjects/CombinedGame'
